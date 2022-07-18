6fade4338444299d7d52301b3e5d1dc59ed256d4
bruno landim <brunolandim13@hotmail.com>
2022-07-18 11:21:56 -0300

    Add func para verificar tipo de arquivo

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,18 +1,33 @@
 import csv
+import json
+import pandas as pd
+import xml
 from ..reports.simple_report import SimpleReport
 from ..reports.complete_report import CompleteReport
 
 class Inventory():
     @staticmethod
-    def import_data(csv_data,report):
-        if report == "simples":
-            with open(csv_data, encoding="utf-8") as file:
+    def get_file_type(path):
+        if 'csv' in path:
+            with open(path, encoding="utf-8") as file:
                 lista = csv.DictReader(file)
                 lista_de_items = [item for item in lista]
-                return SimpleReport.generate(lista_de_items)
+                return lista_de_items
 
+        elif 'json' in path:
+            with open(path, encoding="utf-8") as file:
+                lista = json.load(file)
+                lista_de_items = [item for item in lista]
+                return lista_de_items
+        
+        elif 'xml' in path:
+            file = pd.read_xml(path)
+            lista_de_items = file.to_dict('records')
+            return lista_de_items
+
+    @staticmethod
+    def import_data(path,report):
+        if report == "simples":
+            SimpleReport.generate(Inventory.get_file_type(path))
         elif report == 'completo':
-            with open(csv_data, encoding="utf-8") as file:
-                lista = csv.DictReader(file)
-                lista_de_items_completa = [item for item in lista]
-                return CompleteReport.generate(lista_de_items_completa)
+            CompleteReport.generate(Inventory.get_file_type(path))
