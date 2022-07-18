e3235361b5ea11cb60cd4da4e96c4937aacceb58
bruno landim <brunolandim13@hotmail.com>
2022-07-17 21:23:32 -0300

    Ajuste de identação

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -12,10 +12,10 @@ def test_cria_produto():
         instrucoes_de_armazenamento="Manter longe do Thanos",
     )
 
-        assert produto.id == 1
-        assert produto.nome_do_produto == "Marreta Biônoca"
-        assert produto.nome_da_empresa == "Chapolin"
-        assert produto.data_de_fabricacao == "13/01/2000"
-        assert produto.data_de_validade == "13/01/2022"
-        assert produto.numero_de_serie == "131506"
-        assert produto.instrucoes_de_armazenamento == "Manter longe do Thanos"
+    assert produto.id == 1
+    assert produto.nome_do_produto == "Marreta Biônoca"
+    assert produto.nome_da_empresa == "Chapolin"
+    assert produto.data_de_fabricacao == "13/01/2000"
+    assert produto.data_de_validade == "13/01/2022"
+    assert produto.numero_de_serie == "131506"
+    assert produto.instrucoes_de_armazenamento == "Manter longe do Thanos"
